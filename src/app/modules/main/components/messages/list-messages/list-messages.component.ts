d59528d6a1c052c53c2c2f6bd930ec6a8e9e50e8
Ahmed Saber <ahmed.saber@qorrectassess.com>
2025-03-01 22:33:56 +0200

    update message list page

--- src/app/modules/main/components/messages/list-messages/list-messages.component.ts
+++ src/app/modules/main/components/messages/list-messages/list-messages.component.ts
@@ -4,20 +4,57 @@ const messages = [
   {
     date: '2021-01-31 Sunday',
     hour: '11:36 AM',
-    message: {
-      content:
-        'الأخوة أعضاء اللجنة الفنية لحماية البيئة بمدينة الرياضالسلام عليكم ورحمة الله وبركاته دعوتكم لحضور: sadasdasadsadsads',
-      date: '2021-01-31', 
-      hour: '03:00AM', 
-      location: 'حي السفارات - قصر الثقافة'
-    },
-    receiversList:[
+    messages: [
       {
-        photoPath : 'assests/images', 
-        name:'admin'
-      }
+        content:
+          'الأخوة أعضاء اللجنة الفنية لحماية البيئة بمدينة الرياضالسلام عليكم ورحمة الله وبركاته دعوتكم لحضور: sadasdasadsadsads',
+        date: '2021-01-31',
+        hour: '03:00AM',
+        location: 'حي السفارات - قصر الثقافة',
+        receiversList: [
+          {
+            photoPath: 'assets/images/user-image.jpg',
+            name: 'admin',
+          },
+        ],
+        messageStatus: 'لم يتم الاتصال بالخادم',
+        
+      },
+      {
+        content:
+          'الأخوة أعضاء اللجنة الفنية لحماية البيئة بمدينة الرياضالسلام عليكم ورحمة الله وبركاته دعوتكم لحضور: sadasdasadsadsads',
+        date: '2021-01-31',
+        hour: '03:00AM',
+        location: 'حي السفارات - قصر الثقافة',
+        receiversList: [
+          {
+            photoPath: 'assets/images/user-image.jpg',
+            name: 'admin',
+          },
+        ],
+        messageStatus: 'لم يتم الاتصال بالخادم',
+      },
     ],
-    messageStatus: 'لم يتم الاتصال بالخادم'
+  },
+  {
+    date: '2021-01-31 Sunday',
+    hour: '11:36 AM',
+    messages: [
+      {
+        content:
+          'الأخوة أعضاء اللجنة الفنية لحماية البيئة بمدينة الرياضالسلام عليكم ورحمة الله وبركاته دعوتكم لحضور: sadasdasadsadsads',
+        date: '2021-01-31',
+        hour: '03:00AM',
+        location: 'حي السفارات - قصر الثقافة',
+        receiversList: [
+          {
+            photoPath: 'assets/images/user-image.jpg',
+            name: 'admin',
+          },
+        ],
+        messageStatus: 'لم يتم الاتصال بالخادم',
+      },
+    ],
   },
 ];
 
@@ -27,5 +64,5 @@ const messages = [
   styleUrl: './list-messages.component.scss',
 })
 export class ListMessagesComponent {
-  messagesList = messages
+  messagesList = messages;
 }
