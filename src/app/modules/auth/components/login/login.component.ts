704e34903e98fe2d8a562677d24ab09ee15400eb
Karim Ali Saber <KSaber@octiga.com>
2025-03-25 14:34:25 +0200

    adding site key

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { SwaggerService } from '../../../../swagger/swagger.service';
 
